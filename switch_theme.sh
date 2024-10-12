f05c27a6b634f6d0eecfe7ef0c6512aa3e3b1cb1
fbrit005 <149694433+fbrit005@users.noreply.github.com>
2024-10-11 23:32:19 -0400

    Update switch_theme.sh

--- switch_theme.sh
+++ switch_theme.sh
@@ -7,7 +7,7 @@ fi
 
 # Make input lower case
 theme=$(echo "$1" | tr '[:upper:]' '[:lower:]')
-url="https://bootswatch.com/4/lumen/bootstrap.css"
+url="https://bootswatch.com/4/$theme/bootstrap.css"
 
 if ! command -v curl &> /dev/null
 then
